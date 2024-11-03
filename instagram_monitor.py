e4819c2e46299c4d18e587d5f34d77ed4421d886
misiektoja <misiektoja-github@rm-rf.ninja>
2024-11-03 19:06:43 +0100

    Fixed wrong version number

--- instagram_monitor.py
+++ instagram_monitor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Author: Michal Szymanski <misiektoja-github@rm-rf.ninja>
-v1.6
+v1.5
 
 OSINT tool implementing real-time tracking of Instagram users activities and profile changes:
 https://github.com/misiektoja/instagram_monitor/
@@ -15,7 +15,7 @@ python-dateutil
 requests
 """
 
-VERSION = 1.6
+VERSION = 1.5
 
 # ---------------------------
 # CONFIGURATION SECTION START
