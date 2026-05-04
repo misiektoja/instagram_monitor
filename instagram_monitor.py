d29940c50823d0ab7cd2595a82672724dae400bf
tomballgithub <jeoff@krontz.com>
2026-05-03 23:49:57 -0500

    Updated comment

--- instagram_monitor.py
+++ instagram_monitor.py
@@ -182,7 +182,7 @@ ENABLE_JITTER = False
 JITTER_VERBOSE = False
 
 # Optional Privacy Substitutions
-# This allows you to substitute any string for another in all messaging, logging, webhooks, and emails.
+# This allows you to substitute any string for another in all messaging, logging, webhooks, emails, and both dashboards.
 # For instance, you may want to change a particular Instagram username to a more friendly name, or you could mask a name.
 # 
 # Provide a list of (search, replace) tuples. Any search term will be substituted with the replace term.
